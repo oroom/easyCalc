daac3e7eb9bd4d3329de896c3c6ca06a33b673f8
Dzmitry Navak <navakdzmitry@gmail.com>
2017-11-24 22:34:14 +0300

    initial calculator after first lection.

---
+++ Calculator/ViewController.swift
@@ -0,0 +1,36 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by oroom on 11/24/17.
+//  Copyright © 2017 Dzmitry Novak. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var resultArea: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func didgitButtonTapped(_ sender: Any) {
+        let button = sender as! UIButton
+        let buttonText = button.titleLabel!.text!
+        if resultArea.text! == "0" {
+            resultArea.text! = buttonText
+        }
+        else {
+            resultArea.text! += buttonText
+        }
+    }
+}
+
