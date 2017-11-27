3c6bf3942ff5d02a2f0043bd206409ab9e44e0cc
Artyom Sadyrin <artsiomsadyryn@gmail.com>
2017-11-27 22:58:05 +0300

    Moved calculator logic in separate file .swift

---
+++ Calculator/Calculator.swift
@@ -0,0 +1,57 @@
+//
+//  Calculator.swift
+//  Calculator
+//
+//  Created by Artsiom Sadyryn on 11/27/17.
+//  Copyright © 2017 Dzmitry Novak. All rights reserved.
+//
+
+import Foundation
+
+enum CalculatorOperation {
+    case plus
+    case minus
+    case multiply
+    case divide
+}
+
+class Calcucator {
+    var result: Int?
+    var inputValue: Int = 0
+    var nextOperation: CalculatorOperation?
+    
+    func nextNumber(_ nextNumber: Int) -> Int {
+        switch nextOperation! {
+        case .plus:
+            plus(inputValue, nextNumber)
+        case .minus:
+            minus(inputValue, nextNumber)
+        case .multiply:
+            multiply(inputValue, nextNumber)
+        case .divide:
+            divide(inputValue, nextNumber)
+//        default:
+//            fatalError("Something gone wrong with nextOpetation")
+        }
+        //            nextOperation = nil
+        //            inputValue = result!
+        return result!
+    }
+    
+    func plus(_ firstNumber: Int, _ secondNumber: Int) -> Int {
+        result = firstNumber + secondNumber
+        return result!
+    }
+    func minus(_ firstNumber: Int, _ secondNumber: Int) -> Int {
+        result = firstNumber - secondNumber
+        return result!
+    }
+    func multiply(_ firstNumber: Int, _ secondNumber: Int) -> Int {
+        result = firstNumber * secondNumber
+        return result!
+    }
+    func divide(_ firstNumber: Int, _ secondNumber: Int) -> Int {
+        result = firstNumber / secondNumber
+        return result!
+    }
+}
