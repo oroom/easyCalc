1e71b3c2bfe44f10d0898d36abfca58b70d1a841
Vlad <vlad.gnilozyb@gmail.com>
2017-11-30 22:07:11 +0300

    no message

--- Calculator/CalculatorLogic.swift
+++ Calculator/CalculatorLogic.swift
@@ -7,3 +7,52 @@
 //
 
 import Foundation
+
+enum Operation{
+    case addition
+    case substract
+    case multi
+}
+
+class Calculator{
+    var result: Int?
+    var inputValue: Int = 0
+    var operation: Operation?
+    
+    func nextNumber (_ nextNumber: Int) -> Int {
+        if let operation = operation{
+        switch operation {
+        case .addition:
+            result = addition (inputValue, nextNumber)
+        case .substract:
+            result = substract (inputValue, nextNumber)
+        case .multi:
+            result = multi (inputValue,nextNumber)
+        }
+        return result!
+    }
+        else {
+            return inputValue
+        }
+    }
+    
+    
+    
+    func addition (_ firstNumber: Int,_ nextNumber: Int) -> Int {
+        result = firstNumber + nextNumber
+        return  result!
+    }
+    
+    
+    func substract (_ firstNumber: Int,_ nextNumber: Int) -> Int {
+        result = firstNumber - nextNumber
+        return result!
+    }
+    
+    
+    func multi (_ firstNumber: Int,_ nextNumber: Int) -> Int {
+        result = firstNumber * nextNumber
+        return result!
+    }
+}
+
--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -9,31 +9,75 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     @IBOutlet weak var resultArea: UILabel!
+    var calc = Calculator()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
     @IBAction func didgitButtonTapped(_ sender: Any) {
-        let button = sender as! UIButton
-        let buttonText = button.titleLabel!.text!
-        if resultArea.text! == "0" {
-            resultArea.text! = buttonText
+        guard let button = sender as? UIButton else {
+            return
         }
-        else {
-            resultArea.text! += buttonText
+        
+        
+        if let buttonText = button.titleLabel?.text , var resultText = resultArea?.text{
+            if resultText == "0" {
+                resultText = buttonText
+            }
+            else {
+                resultText += buttonText
+            }
+            resultArea.text = resultText
         }
+        
+        
     }
     
-
+    
+    @IBAction func operationTapped(_ sender: Any) {
+        guard  let button = sender as? UIButton else {
+            return
+        }
+        if let number = resultArea?.text, let inputValue = Int(number){
+            calc.inputValue = inputValue
+        if  let buttonText = button.titleLabel?.text{
+            switch buttonText {
+            case "-":
+                calc.operation = .substract
+            case "+":
+                calc.operation = .addition
+            case "×":
+                calc.operation = .multi
+            default:
+                return ()
+            }
+        }
+        }
+        resultArea.text = "0"
+    }
+    
+    
+    @IBAction func resultCalc(_ sender: Any) {
+        let nextNumber = Int(resultArea.text!)
+        resultArea.text = String(calc.nextNumber(nextNumber!))
+        
+        
+    }
+    
+    
+    
+    @IBAction func cleanResult(_ sender: Any) {
+        resultArea.text = "0"
+    }
     
 }
 
